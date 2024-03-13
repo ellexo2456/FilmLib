47bf3015a849739ad30290d2495b1e491bbc88e1
Alexey <ax.chinaev@yandex.ru>
2024-03-13 20:05:29 +0300

    add gitignore

--- cmd/app/app.go
+++ cmd/app/app.go
@@ -1,5 +1,18 @@
 package app
 
-func StartServer() {
+import (
+	"log"
+	"net/http"
+)
 
+func StartServer() {
+	mux := http.NewServeMux()
+
+	err := http.ListenAndServe(serverPort, mux)
+	if err != nil {
+		log.Fatal("can`t start sever")
+		//logs.LogFatal(logs.Logger, "auth", "main", err, err.Error())
+	}
+
+	//logs.Logger.Info("auth http server stopped")
 }
